c87d9cafe789830f55c7729d4aced4c47ff5710a
Pavel <elena71vd@gmail.com>
2021-01-16 18:51:40 +0200

    do task

---
+++ assets/js/index.js
@@ -0,0 +1,48 @@
+'use strict';
+
+class RangeValidator{
+  constructor(from, to) {
+    this._from = from;
+    this._to = to; 
+  }
+
+  get from() {
+    return this._from;
+  }
+
+  set from(from) {
+    if (typeof from === 'number') {
+      this._from = from;
+      return;
+    }
+    throw new TypeError('Argument is not a Number');
+  }
+
+  get to() {
+    return this._to;
+  }
+    
+  set to(to) {
+    if (typeof to === 'number') {
+      this._to = to;
+      return;
+    }
+    throw new TypeError('Argument is not a Number');
+  }
+
+  get range() {
+    if (this._from <= this._to) {
+      return [this._from, this._to];
+    }
+    throw new RangeError('"to" can not be less than "from"');
+  }
+
+  validate(number) {
+    if (number >= this._from && number <= this._to) {
+      return number;
+    }
+    throw new RangeError('Number is out of range');
+  }
+}
+
+const q = new RangeValidator(1, 10);
